72bead675b3c4041b561c312c3ea29ae28535308
Orange <0ae@protonmail.com>
2023-02-11 00:16:32 +0900

    miniVideoのサイズOKとクリック時スクロールの準備

--- src/components/top/left/index.tsx
+++ src/components/top/left/index.tsx
@@ -1,7 +1,13 @@
 import {
   component$,
+  createContext,
+  noSerialize,
   useClientEffect$,
+  useContext,
+  useContextProvider,
+  useServerMount$,
   useSignal,
+  useStore,
   useStylesScoped$,
 } from "@builder.io/qwik";
 import styles from "./index.css?inline";
@@ -9,91 +15,87 @@ import SearchIcon from "~/icons/search";
 import FilterIcon from "~/icons/filter";
 import SeparateIcon from "~/icons/separate";
 import Item from "~/components/blog/item";
-import { RequestHandler } from "@builder.io/qwik-city";
+import { RequestHandler, useContent } from "@builder.io/qwik-city";
 import { blogType, getBlogs } from "~/library/getBlog";
 import anime from "lib/anime.es.js";
-
-/*
-
-// Core modules imports are same as usual
-import { Navigation, Pagination } from 'swiper';
-// Direct React component imports
-import { Swiper, SwiperSlide } from 'swiper/react/swiper-react.js';
-
-*/
 
 import Swiper from "swiper";
 import { Navigation, Pagination, Scrollbar } from "swiper";
 
-/*
-// Styles must use direct files imports
-import 'swiper/swiper.scss'; // core Swiper
-import 'swiper/modules/navigation/navigation.scss'; // Navigation module
-import 'swiper/modules/pagination/pagination.scss'; 
-*/
+export const activeIndexContext = createContext('activeIndex');
+
 export default component$(() => {
   useStylesScoped$(styles);
 
   const containerRef = useSignal<Element>();
-  const containerHeight = useSignal<number>(0);
-
+  const activeIndex = useSignal<number>(0);
+  const containerSizes = useStore({
+    height:0,
+    width:0,
+    minus3_5xHeight: 0,
+    minus3_5xWidth: 0,
+  });
+  useContextProvider(activeIndexContext, activeIndex);
   useClientEffect$(() => {
     if(containerRef.value?.clientHeight && containerRef.value?.clientHeight != 0){
-      containerHeight.value = containerRef.value?.clientHeight;
+      containerSizes.height = containerRef.value?.clientHeight;
+      containerSizes.width = (containerSizes.height/18)*9;
+      containerSizes.minus3_5xHeight = containerSizes.height / 3.5;
+      containerSizes.minus3_5xWidth = containerSizes.width / 3.5;
     }
   });
 
   const testVideos = [
     {
-      title: "A",
+      title: "A1",
     },
     {
-      title: "B",
+      title: "B2",
     },
     {
-      title: "C",
+      title: "C3",
     },
     {
-      title: "D",
+      title: "D4",
     },
     {
-      title: "E",
+      title: "E5",
+    },
+    {
+      title: "F6",
+    },
+    {
+      title: "G7",
+    },
+    {
+      title: "H8",
     },
   ];
+
   return (
     <div class="container" ref={containerRef}>
-      { (containerHeight.value && containerRef.value?.clientHeight) ? <BigVideoContainer containerHeight={containerHeight.value} /> : <div />}
-      <div class="miniVideosContainer">
-        {testVideos.map((miniVideo) => {
-         return <div class="miniVideoContainer"></div>;
-        })}
-      </div>
+      { (containerSizes.height && containerRef.value?.clientHeight) ? 
+      <>
+      <BigVideoContainer data={testVideos} containerSizes={containerSizes} />
+      <MiniVideoContainer data={testVideos} containerSizes={containerSizes} />
+      </>
+       : <div />
+       }
+
     </div>
   );
 });
 
-export const BigVideoContainer = component$(({containerHeight}) => {
+export const BigVideoContainer = component$(({data, containerSizes}) => {
   useStylesScoped$(styles);
-  const testVideos = [
-    {
-      title: "A",
-    },
-    {
-      title: "B",
-    },
-    {
-      title: "C",
-    },
-    {
-      title: "D",
-    },
-    {
-      title: "E",
-    },
-  ];
-  useClientEffect$(() => {
+  //const swiper = useSignal();
+  const activeIndex = useContext(activeIndexContext);
+  console.log("ais", activeIndex);
+  
+  useServerMount$(() => {
     let swiper = new Swiper(".bigVideosContainer", {
       direction: 'vertical',
+      loop:true,
       autoplay: {
         delay: 5000,
       },
@@ -103,23 +105,40 @@ export const BigVideoContainer = component$(({containerHeight}) => {
         prevEl: ".swiper-button-prev",
       },
     });
+       console.log("LIJFOIJODIJORJIOEIJOTIJRO");
+       //swiper.slideTo(activeIndex, 1500, true);
   });
 
 return (
-  <div class="bigVideosContainer" style={{height: containerHeight.toString() + "px"}}>
+  <div class="bigVideosContainer" style={{height: containerSizes.height.toString() + "px", width: containerSizes.width.toString() + "px"}}>
   <div class="swiper-wrapper">
-    {testVideos.map((bigVideo) => {
+    {data.map((bigVideo) => {
       return (
         <div class="bigVideoContainer swiper-slide">
           <div class="titleContainer">
             <p class="title">
               {bigVideo.title}
+              {activeIndex.value}
             </p>
           </div>
         </div>
       );
     })}
   </div>
+</div>
+);
+});
+
+export const MiniVideoContainer = component$(({data, containerSizes}) => {
+  useStylesScoped$(styles);
+  const activeIndex = useContext(activeIndexContext);
+return (
+  <div class="miniVideosContainer" style={{height: containerSizes.height.toString() + "px"}}>
+  {data.map((miniVideo, i) => {
+   return <div class="miniVideoContainer" onClick$={() => {activeIndex.value = i;}} style={{height: containerSizes.minus3_5xHeight.toString() + "px", width: containerSizes.minus3_5xWidth.toString() + "px"}}>
+    <p>{i}</p>
+   </div>;
+  })}
 </div>
 );
 });
